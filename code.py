bb2050ce679dbe7ac2ed01b6472b3bb98a953425
misha-solntcev <misha.solntcev@gmail.com>
2022-02-19 08:10:33 +0800

    Transform to a list

--- code.py
+++ code.py
@@ -1,9 +1,13 @@
-x = 1
-y = 2
-SUM = 0
-for i in range(0,10):
-    SUM = x + y
-    x = y
-    y = SUM
-    print(SUM)
-    
+F = []
+f_0 = 0
+F.append(f_0)
+f_1 = 1
+F.append(f_1)
+f_2 = 1
+F.append(f_2)
+for i in range(13):
+    f_n = f_1 + f_2
+    f_1 = f_2
+    f_2 = f_n
+    F.append(f_n)
+print(F)
